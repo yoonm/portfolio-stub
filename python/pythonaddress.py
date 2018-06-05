33dfed36e03e6af7c014de1718ad2daae16f6b00
yoonm <yoon.mihyun@rutgers.edu>
2018-06-05 14:55:24 -0400

    Added conversion.py

---
+++ python/pythonaddress.py
@@ -0,0 +1,11 @@
+personal_info = {"address": "123 Tony Street",
+                "city": "Philadelphia",
+                "state": "PA",
+                "zip code": "12345"}
+
+print(personal_info["address"]
+        + "\n"
+        + personal_info["city"]
+        + ", " + personal_info["state"]
+        + " "
+        + personal_info["zip code"])
