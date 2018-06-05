a553439d78304bd61b6c5ef3d0a21f15546e6470
yoonm <yoon.mihyun@rutgers.edu>
2018-06-05 14:19:18 -0400

    Added pythonaddress.py

--- python/my_info.py
+++ python/my_info.py
@@ -1,1 +1,4 @@
-my_info = {"first_name": "Mi Hyun", "last_name": "Yoon", "hometown": "Closter", "favorite_food": "mango"}
+my_info = {"first_name": "Mi Hyun",
+            "last_name": "Yoon",
+            "hometown": "Closter",
+            "favorite_food": "mango"}
