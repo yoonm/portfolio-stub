4df6c1feed0a28c7790d257eb7f54e08d27007a4
yoonm <yoon.mihyun@rutgers.edu>
2018-06-06 11:00:28 -0400

    Added more examples

--- python/author_death.py
+++ python/author_death.py
@@ -1,8 +1,14 @@
 author_year = {"Charles Dickens": "1870",
-            "William Thackeray": "1863",
-            "Anthony Trollope": "1882",
-            "Gerard Manley Hopkins": "1889"}
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889"}
 
 for author, year in author_year.items():
     print(author + " kicked the bucket in " + year + ".")
-    
+
+## using a method:
+# def print_the_names(name, year):
+#   print(author + " kicked the bucket in " + year + ".")
+#
+# for author, year in author_year.items():
+#   print_the_names(author, year)
---
+++ python/timetravel.py
@@ -0,0 +1,10 @@
+origin_year = 2030
+
+if origin_year < 1900:
+    print("Hi matey!")
+elif origin_year >= 1900 and origin_year <= 2020:
+    print("Hello!")
+else:
+    print("Take me to your leader.")
+
+ 
