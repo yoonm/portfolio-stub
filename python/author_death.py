a2b78404341cc8cb6d8d521221917de39f7f9a4b
yoonm <yoon.mihyun@rutgers.edu>
2018-06-06 10:18:05 -0400

    Added loop using dictionary

---
+++ python/author_death.py
@@ -0,0 +1,8 @@
+author_year = {"Charles Dickens": "1870",
+            "William Thackeray": "1863",
+            "Anthony Trollope": "1882",
+            "Gerard Manley Hopkins": "1889"}
+
+for author, year in author_year.items():
+    print(author + " kicked the bucket in " + year + ".")
+    
